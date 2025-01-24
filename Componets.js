fc6025cb009577b19c19bc103dd44c7eeade8a64
Abdurrehman49 <engrabdurrehmanuet@gmail.com>
2025-01-24 11:30:49 +0500

    I delete the scr folder

---
+++ Componets.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Componets = () => {
+    return (
+        <View>
+            <Text>Componets</Text>
+        </View>
+    )
+}
+
+export default Componets
