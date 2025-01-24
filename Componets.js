6abc679d57a05ac1096a6fbaf59252aa78fa355a
Abdurrehman49 <engrabdurrehmanuet@gmail.com>
2025-01-24 12:10:02 +0500

    updated the component 3 times

--- Componets.js
+++ Componets.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Componets = () => {
     return (
         <View>
-            <Text>Componets is updated</Text>
+            <Text>Componets is updated 3 times</Text>
         </View>
     )
 }
