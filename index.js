45b4361c8f331b00e1ac11841528f10bf5633a96
Abdurrehman49 <engrabdurrehmanuet@gmail.com>
2025-01-24 10:42:32 +0500

    update the index file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import React from 'react'
 const index = () => {
     return (
         <View>
-            <Text>Hello Everyone</Text>
+            <Text>Hello Everyone this index file</Text>
         </View>
     )
 }
